03e822602910b8b35151ba8fd017ffbeeaf99cf5
Nenad Misic <nenad.misic997@gmail.com>
2020-03-19 03:08:44 +0100

    wtf

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="cinemax">
     <App />
   </BrowserRouter>
 , document.getElementById('root'));
