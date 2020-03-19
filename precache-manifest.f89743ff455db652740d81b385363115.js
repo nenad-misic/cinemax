3c34621c6cfb45381dbfe170700bd0be5fb2044a
Nenad Misic <nenad.misic997@gmail.com>
2020-03-19 01:07:59 +0100

    Updates

---
+++ precache-manifest.f89743ff455db652740d81b385363115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5812c463d8c7e395da5d2833dc4f693a",
+    "url": "/cinemax/index.html"
+  },
+  {
+    "revision": "672bfcdc362b7763eaaa",
+    "url": "/cinemax/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "772837091b05ac7202a3",
+    "url": "/cinemax/static/css/main.132c353e.chunk.css"
+  },
+  {
+    "revision": "672bfcdc362b7763eaaa",
+    "url": "/cinemax/static/js/2.662d8d59.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cinemax/static/js/2.662d8d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "772837091b05ac7202a3",
+    "url": "/cinemax/static/js/main.e8639768.chunk.js"
+  },
+  {
+    "revision": "8a83ed248508ab7f62a8",
+    "url": "/cinemax/static/js/runtime-main.eb9f3374.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ebc7e4189d2a3bd34addabe6129644b.js"
+  "/cinemax/precache-manifest.f89743ff455db652740d81b385363115.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cinemax/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
