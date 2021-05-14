29f5d768160aa7113c7c1986ccb3bcb7acb4741f
Nenad Misic <nenad.misic997@gmail.com>
2021-05-14 10:28:36 +0200

    'try'

--- precache-manifest.e7fcc724f802a81155c3acfbd6d315c6.js
+++ precache-manifest.e7fcc724f802a81155c3acfbd6d315c6.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "01d103854eb052d0508a03ebb53292c8",
-    "url": "/index.html"
+    "url": "cinema/index.html"
   },
   {
     "revision": "672bfcdc362b7763eaaa",
-    "url": "/static/css/2.1fd2b905.chunk.css"
+    "url": "cinema/static/css/2.1fd2b905.chunk.css"
   },
   {
     "revision": "efb2da6369c373c9ba3c",
-    "url": "/static/css/main.d498bfb3.chunk.css"
+    "url": "cinema/static/css/main.d498bfb3.chunk.css"
   },
   {
     "revision": "672bfcdc362b7763eaaa",
-    "url": "/static/js/2.662d8d59.chunk.js"
+    "url": "cinema/static/js/2.662d8d59.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/static/js/2.662d8d59.chunk.js.LICENSE.txt"
+    "url": "cinema/static/js/2.662d8d59.chunk.js.LICENSE.txt"
   },
   {
     "revision": "efb2da6369c373c9ba3c",
-    "url": "/static/js/main.7003b71f.chunk.js"
+    "url": "cinema/static/js/main.7003b71f.chunk.js"
   },
   {
     "revision": "eaeb41e65fd1097989e3",
-    "url": "/static/js/runtime-main.d850afaf.js"
+    "url": "cinema/static/js/runtime-main.d850afaf.js"
   }
 ]);
